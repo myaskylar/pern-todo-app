60aa324b5918508dcf7cca76a496f8c7e15da68c
Mya <myaskylar14@gmail.com>
2022-05-15 14:31:51 +0100

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
-// app.use(express.static("./client/build"));
+app.use(express.static("./client/build"));
 
 //ROUTES//
 
